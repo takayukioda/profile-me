72487e7822a4de6161657c2efc63d09011561adf
Takayuki ODA <dev@da0shi.com>
2014-05-15 10:08:49 +0900

    add some initial files

---
+++ signup.php
@@ -0,0 +1,15 @@
+<?php require_once 'bootstrap.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf8" />
+	<title>ProfileMe::Signup</title>
+</head>
+<body>
+	<form method="post" action="user-create">
+		<input name="username" type="text"/>
+		<input name="mail" type="text"/>
+		<input name="username" type="text"/>
+	</form>
+</body>
+</html>
