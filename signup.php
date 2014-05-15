61475d2d22254738aec1b45084957e21f7e84ccc
Takayuki Oda <dev@da0shi.com>
2014-05-15 14:28:03 +0900

    update signup page

--- signup.php
+++ signup.php
@@ -6,10 +6,12 @@
 	<title>ProfileMe::Signup</title>
 </head>
 <body>
-	<form method="post" action="user-create">
-		<input name="username" type="text"/>
-		<input name="mail" type="text"/>
-		<input name="username" type="text"/>
+	<form method="post" action="user-create.php">
+		<table>
+		<tr>
+		<th>名前:</th><td><input name="username" type="text"/></td>
+		<th>メールアドレス:</th><td><input name="mail" type="text"/></td>
+		<th>パスワード:</th><td><input name="passwd" type="password"/></td>
 	</form>
 </body>
 </html>
---
+++ user-create.php
@@ -0,0 +1,3 @@
+<?php
+require_once 'bootstrap.php';
+require_once 'models/users.php';
