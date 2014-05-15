d5ee708c0c0a50e791f1041b6e6bd3d3d2fc016d
Takayuki ODA <dev@da0shi.com>
2014-05-15 22:58:54 +0900

    complete basic part of Model class

--- config/database.php
+++ config/database.php
@@ -2,11 +2,39 @@
 
 class DatabaseConfig
 {
-	public $default = array(
+	const ENV = 'development';
+	public $default = null;
+	protected $template = array(
+		'username' => 'root',
+		'hostname' => 'localhost',
+		'password' => 'root',
+		'database' => 'root',
+		'encoding' => 'utf8',
+		'port' => 3306,
+		'socket' => '',
+		'pconnect' => false,
+	);
+	protected $production = array(
+		'username' => '',
+		'hostname' => '',
+		'password' => '',
+		'database' => '',
+	);
+	protected $development = array(
 		'username' => 'profileme',
 		'hostname' => 'localhost',
-		'password' => 'profile@localhost',
+		'password' => 'profile',
 		'database' => 'profileme',
-		'encoding' => 'utf-8',
+		'socket' => '/opt/local/var/run/mysql5/mysqld.sock',
 	);
+
+	public function __construct ()
+	{
+		$env = static::ENV;
+		if (property_exists(get_called_class(), $env)) {
+			$this->default = array_merge($this->template, $this->{$env});
+		} else {
+			$this->default = $this->template;
+		}
+	}
 }
--- models/model.php
+++ models/model.php
@@ -1,16 +1,15 @@
 <?php
-
 class Model
 {
-	protected static $_initialized = false;
-	protected static $_connection = array();
+	protected static $_connection = null;
 	protected static $_table_name = null;
+	protected static $_table_name_cached = array();
 	protected static $_primary_key = array('id');
 	protected static $_properties = array();
 	protected static $_properties_cached = array();
 	protected static $_is_new = true;
 	protected $_data = array();
-	protected $_original_data = array();
+	protected $_original = array();
 	protected $_custom_data = array();
 
 	public function __isset ($prop)
@@ -52,7 +51,23 @@ class Model
 		} else {
 			if (func_num_args() < 2) throw new \InvalidArgumentException;
 			if (in_array($prop, static::primary_key())) throw new \InvalidArgumentException;
-			if (array_key_exists($prop, static::properties())) {
+			$properties = static::properties();
+			if (array_key_exists($prop, $properties)) {
+				if (array_key_exists('type', $properties[$prop])) {
+					switch ($properties[$prop]['type']) {
+					case 'boolean':
+						$value = (boolean) $value; break;
+					case 'int':
+						$value = (int) $value; break;
+					case 'double':
+						$value = (double) $value; break;
+					case 'varchar': // fall-through
+					case 'text':
+						$value = (string) $value; break;
+					default:
+						$value = (string) $value; break;
+					}
+				}
 				$this->_data[$prop] = $value;
 			} else {
 				$this->_custom_data[$prop] = $value;
@@ -62,9 +77,13 @@ class Model
 		return $this;
 	}
 
-	protected function __construct (array $data = array(), $new = true)
+	public function __construct (array $data = array(), $new = true)
 	{
-		if (! empty($this->_data) {
+		if ($this->connection() === null) {
+			$this->connect();
+		}
+
+		if (! empty($this->_data)) {
 			$data = array_merge($this->data, $data);
 			$this->_data = array();
 			$new = false;
@@ -78,7 +97,7 @@ class Model
 				$this->_data[$prop] = $settings['default'];
 			}
 			$this->_custom_data = $data;
-			
+
 			if ($new === false) {
 				$this->_update_original($this->_data);
 				$this->_is_new = false;
@@ -86,15 +105,59 @@ class Model
 		}
 	}
 
+	protected function connect ()
+	{
+		require_once 'config/database.php';
+		if (class_exists('DatabaseConfig')) {
+			$config = new DatabaseConfig();
+			$config = $config->default;
+			$pdo_config = array(
+				PDO::ATTR_PERSISTENT => $config['pconnect'],
+				PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
+			);
+			if (! empty($config['encoding'])) {
+				$pdo_config[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES ' . $config['encoding'];
+			}
+			if (! empty($config['socket'])) {
+				$dsn = "mysql:host={$config['hostname']};dbname={$config['database']};unix_socket={$config['socket']};";
+			} else {
+				$dsn = "mysql:host={$config['hostname']};dbname={$config['database']};port={$config['port']};";
+			}
+			try {
+				$this->_connection = new PDO (
+					$dsn,
+					$config['username'],
+					$config['password'],
+					$pdo_config
+				);
+			} catch  (PDOException $e) {
+				throw new \Exception ($e->getMessage());
+			}
+		}
+	}
+
 	public static function instance (array $data = array(), $new = true)
 	{
-		if (
 		return new static($data, $new);
 	}
 
 	public static function primary_key ()
 	{
 		return static::$_primary_key;
+	}
+
+	public static function table ()
+	{
+		$class = get_called_class();
+		if (property_exists($class, static::$_table_name_cached)) return static::$_table_name_cached[$class];
+		if (property_exists($class, '_table_name')) {
+			static::$_table_name_cached[$class] = $this->_table_name;
+		} else {
+			static::$_table_name_cached[$class] = strtolower($class);
+		}
+		return static::$_table_name_cached[$class];
 	}
 
 	public static function properties ()
@@ -120,5 +183,38 @@ class Model
 
 		static::$_properties_cached[$class] = $properties;
 		return static::$_properties_cached[$class];
+	}
+
+	public function connection ()
+	{
+		$class = get_called_class();
+
+		return property_exists($class, '_connection') ? static::$_connection : null;
+	}
+
+	public function is_changed ($prop)
+	{
+		$properties = static::properties();
+		$property = (array) $property ?: array_keys($properties);
+		foreach ($properties as $p) {
+			if (! isset($properties[$p])) throw new \OutOfBoundsException("Unknown property {$p}");
+
+			if (array_key_exists($p, $this->_original)) {
+				if (array_key_exists('type', $properties) and $properties[$p]['type'] == 'int') {
+					if ($this->{$p} != $this->_original[$p]) return true;
+				} elseif ($this->{$p} !== $this->_original[$p]) {
+					return true;
+				}
+			} elseif (array_key_exists($p, $this->_data)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	protected function _update_original ($original = null) {
+		$original = is_null($original) ? $this->_data : $original;
+		$this->_original = $original;
 	}
 }
