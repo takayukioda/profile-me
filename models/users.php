33d65eaa3d08c5ee2003f4342372ea8f7630a7c9
Takayuki ODA <dev@da0shi.com>
2014-05-16 13:35:41 +0900

    update user model

--- models/users.php
+++ models/users.php
@@ -1,5 +1,36 @@
 <?php
 
-class Users
+class Users extends Model
 {
+	protected static $_table_name = 'users';
+	protected static $_properties = array(
+		'id' => array(
+			'type' => 'int',
+		),
+		'username' => array(
+			'type' => 'varchar',
+		),
+		'mail' => array(
+			'type' => 'varchar',
+		),
+		'password' => array(
+			'type' => 'varchar',
+		),
+		'self_promotion' => array(
+			'type' => 'text',
+		),
+		'created_at' => array(
+			'type' => 'datetime',
+		),
+		'updated_at' => array(
+			'type' => 'datetime',
+		),
+	);
+
+	public function create ()
+	{
+		if (! $this->is_new()) return false;
+
+		$sql = sprintf("INSERT INTO (%s) VALUES (%s);");
+	}
 }
