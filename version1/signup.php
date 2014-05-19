4c6eb90c37487faee82aa2260d64312915004646
da0shi <dev@da0shi.com>
2014-05-20 02:04:48 +0900

    update signup function

--- version1/signup.php
+++ version1/signup.php
@@ -4,14 +4,27 @@
 <head>
 	<meta charset="utf8" />
 	<title>ProfileMe::Signup</title>
+	<link rel="stylesheet" href="css/common.css"/>
 </head>
 <body>
 	<form method="post" action="user-create.php">
+		<span class="invalid"><?php echo $_SESSION['error']['already'];?></span>
 		<table>
 		<tr>
-		<th>名前:</th><td><input name="username" type="text"/></td>
-		<th>メールアドレス:</th><td><input name="mail" type="text"/></td>
-		<th>パスワード:</th><td><input name="passwd" type="password"/></td>
+		<th>名前:<span class="invalid"><?php echo $_SESSION['error']['username'];?></span></th>
+		<td><input name="username" type="text"/></td>
+		</tr>
+		<tr>
+		<th>メールアドレス:<span class="invalid"><?php echo $_SESSION['error']['mail'];?></span></th>
+		<td><input name="mail" type="text"/></td>
+		</tr>
+		<tr>
+		<th>パスワード:<span class="invalid"><?php echo $_SESSION['error']['passwd'];?></span></th>
+		<td><input name="passwd" type="password"/></td>
+		</tr>
+		</table>
+		<input type="submit" value="Submit" />
 	</form>
 </body>
 </html>
+<?php unset($_SESSION['error']); ?>
